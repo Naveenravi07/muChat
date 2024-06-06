687b9915648919b4574379dc2e7877ba696e5a29
Naveenravi07 <gamerkidnav@gmail.com>
2024-06-07 00:15:03 +0530

    fix

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::net::SocketAddr;
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
+    sync::broadcast::{Receiver, Sender},
 };
 
 #[tokio::main]
@@ -13,7 +14,7 @@ async fn main() -> Result<()> {
     let addr = "0.0.0.0:8080";
     let listener = TcpListener::bind(addr).await?;
     let (tx, _rx): (Sender<(String, SocketAddr)>, Receiver<(String, SocketAddr)>) =
-                    tokio::sync::broadcast::channel(30);
+        tokio::sync::broadcast::channel(30);
 
     loop {
         let tx2 = tx.clone();
@@ -35,12 +36,12 @@ async fn main() -> Result<()> {
                     },
 
                     Ok((message, message_addr)) = rx2.recv() =>  {
-                        if message_addr == sock_addr {
-                            continue;
-                        }
-                        tracing::info!("Receieved {}", message);
-                        s_writer.write_all(message.as_bytes()).await.unwrap();
-                    }
+                             if message_addr == sock_addr {
+                                 continue;
+                             }
+                             tracing::info!("Receieved {}", message);
+                             s_writer.write_all(message.as_bytes()).await.unwrap();
+                         }
 
                 }
             }
