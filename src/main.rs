b49c66dbc43b457af7e8514c2c45e6fa7d1dfbc8
Naveenravi07 <gamerkidnav@gmail.com>
2024-06-07 09:06:58 +0530

    Username feature added

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use anyhow::Result;
-use std::net::SocketAddr;
+use std::{
+    collections::HashMap,
+    net::SocketAddr,
+    str::FromStr,
+    sync::{Arc, RwLock},
+};
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
+    select,
     sync::broadcast::{Receiver, Sender},
 };
 
@@ -14,7 +20,9 @@ async fn main() -> Result<()> {
     let addr = "0.0.0.0:8080";
     let listener = TcpListener::bind(addr).await?;
     let (tx, _rx): (Sender<(String, SocketAddr)>, Receiver<(String, SocketAddr)>) =
-                    tokio::sync::broadcast::channel(30);
+        tokio::sync::broadcast::channel(30);
+    let client_data: Arc<RwLock<HashMap<SocketAddr, String>>> =
+        Arc::new(RwLock::new(HashMap::new()));
 
     loop {
         let tx2 = tx.clone();
@@ -23,24 +31,45 @@ async fn main() -> Result<()> {
         let (mut stream, sock_addr) = listener.accept().await?;
         tracing::info!("INFO: new client connected successfully");
 
+        let client_data_clo = Arc::clone(&client_data);
         tokio::spawn(async move {
             let (s_reader, mut s_writer) = stream.split();
             let mut stream_buff_reader = BufReader::new(s_reader);
+            let mut client_inp = String::new();
+
+            println!("Enter the username \n");
+            s_writer
+                .write("Please Enter your username".as_bytes())
+                .await
+                .unwrap();
+
+            stream_buff_reader.read_line(&mut client_inp).await.unwrap();
+            {
+                client_data_clo
+                    .write()
+                    .unwrap()
+                    .insert(sock_addr, client_inp.trim().to_string());
+            }
+            client_inp.clear();
+            println!("{:?}", client_data_clo);
 
             loop {
-                let mut client_inp = String::new();
-                tokio::select! {
+                select! {
                     _ = stream_buff_reader.read_line(&mut client_inp)=>{
                         tx2.send((client_inp.clone(), sock_addr)).unwrap();
                         client_inp.clear();
                     },
 
                     Ok((message, message_addr)) = rx2.recv() =>  {
-                        tracing::info!("Receieved {}", message);
+                        let client_name = {
+                            let client_nam_reader = client_data_clo.read().unwrap();
+                            client_nam_reader.get(&message_addr).unwrap().clone()
+                        };
+                        tracing::info!("Receieved {:?} from {}", message.as_bytes(),client_name);
                         if message_addr == sock_addr {
                             continue;
                         }
-                        let fm_str = format!(" {} {}",sock_addr.port(),message);
+                        let fm_str = format!(" {} : {}",client_name,message);
                         s_writer.write_all(fm_str.as_bytes()).await.unwrap();
                     }
                 }
