4b1ddc317a2c7da1061a9e8f900ba5716e58b83f
Naveenravi07 <gamerkidnav@gmail.com>
2024-06-15 00:34:51 +0530

    Show announcement messages when someone joins

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ use tokio::{
     net::TcpListener,
     select,
     sync::broadcast::{Receiver, Sender},
-    task::JoinHandle,
 };
 
 const MAX_CLIENT_NAME_LENGTH: usize = 10;
@@ -23,12 +22,13 @@ async fn main() -> Result<()> {
 
     let addr = "0.0.0.0:8080";
     let listener = TcpListener::bind(addr).await?;
-    let (tx, _rx): (Sender<(String, SocketAddr)>, Receiver<(String, SocketAddr)>) =
-        tokio::sync::broadcast::channel(30);
+    let (tx, _rx): (
+        Sender<(String, Option<SocketAddr>)>,
+        Receiver<(String, Option<SocketAddr>)>,
+    ) = tokio::sync::broadcast::channel(30);
     let client_data: Arc<RwLock<HashMap<SocketAddr, String>>> =
         Arc::new(RwLock::new(HashMap::new()));
-    let client_handlers: Arc<RwLock<HashMap<SocketAddr, JoinHandle<_>>>> =
-        Arc::new(RwLock::new(HashMap::new()));
+    //let client_handlers: Arc<RwLock<HashMap<SocketAddr, JoinHandle<_>>>> = Arc::new(RwLock::new(HashMap::new()));
 
     loop {
         let tx2 = tx.clone();
@@ -83,34 +83,39 @@ async fn main() -> Result<()> {
                 .unwrap()
                 .insert(sock_addr, client_inp.trim().to_string());
 
-            let welcome = format!("\n {} Joined the club", client_inp.trim());
+            let welcome = format!("\n {} Joined the club \n", client_inp.trim());
             client_inp.clear();
 
-            tx2.send((welcome, sock_addr)).unwrap();
+            tx2.send((welcome, None)).unwrap();
             loop {
                 select! {
                     _ = stream_buff_reader.read_line(&mut client_inp)=>{
                         client_inp = censor.censor(&client_inp);
-                        tx2.send((client_inp.clone(), sock_addr)).unwrap();
+                        tx2.send((client_inp.clone(), Some(sock_addr))).unwrap();
                         client_inp.clear();
                     },
 
                     Ok((message, message_addr)) = rx2.recv() =>  {
-                        let client_name = {
-                            let client_nam_reader = client_data_clo.read().unwrap();
-                            client_nam_reader.get(&message_addr).unwrap().clone()
+
+                        let fm_str = if let Some(address) = message_addr{
+                            let client_name = {
+                                let client_nam_reader = client_data_clo.read().unwrap();
+                                client_nam_reader.get(&address).unwrap().clone()
+                            };
+                            tracing::info!("Receieved {:?} from {}", message.as_bytes(),client_name);
+                            if address == sock_addr {
+                                continue;
+                            }
+                             format!(" {} : {}",client_name.blue(),message.bright_green())
+                        }else{
+                             format!(" {}",message.bright_green())
                         };
-                        tracing::info!("Receieved {:?} from {}", message.as_bytes(),client_name);
-                        if message_addr == sock_addr {
-                            continue;
-                        }
-                        let fm_str = format!(" {} : {}",client_name.blue(),message.bright_green());
                         s_writer.write_all(fm_str.as_bytes()).await.unwrap();
                         s_writer.flush().await.unwrap();
                     }
                 }
             }
         });
-        client_handlers.write().unwrap().insert(sock_addr, task);
+        //client_handlers.write().unwrap().insert(sock_addr, task);
     }
 }
