bfb3f90cbdb1733fbd93441959b2361b0ca1c409
Naveenravi07 <gamerkidnav@gmail.com>
2024-06-23 11:27:07 +0530

    Alignment changes

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,7 @@
 use iced::{
-     alignment::Horizontal, executor, widget::{column, container, pick_list, row, text, Button, Row, Text, TextInput}, Application, Command, Length, Settings, Theme
+    alignment::{Horizontal, Vertical},
+    executor,
+    widget::{column, container, pick_list, row, text, Button, Row, Text, TextInput}, Application, Command, Length, Settings, Theme,
 };
 use iced_aw::Card;
 
@@ -54,70 +56,78 @@ impl Application for App {
     }
 
     fn view(&self) -> iced::Element<'_, Message> {
-        if self.show_modal {
-            let modal_content = Card::new(
-                Text::new("Server "),
-                container(
-                    column![
-                        row![
+        println!("Open modal closed ");
+        let server_pk_list = {
+            let tags = vec!["Connect".to_string(), "Disconnect".to_string()];
+            let tag_el = pick_list(tags, None::<String>, Message::ServerDropDownAction)
+                .placeholder("Server");
+            tag_el
+        };
+        let self_pk_list = {
+            let tags = vec!["Mute Self".to_string(), "Defean Self".to_string()];
+            let tag_el =
+                pick_list(tags, None::<String>, Message::SelfDropDownAction).placeholder("Self");
+            tag_el
+        };
+
+        let top_bar = row![server_pk_list, self_pk_list].height(50).spacing(10);
+
+        if !self.show_modal {
+            container(top_bar).padding(10).into()
+        } else {
+                let server_conn_modal = container(
+                    Card::new(
+                        Text::new("Server "),
+                        container(
+                            column![
+                            row![
                             text("Server Address"),
                             TextInput::new("Server Address", "Some val")
-                                .on_input(Message::ServerAddrInpuChanged)
-                        ]
-                        .spacing(20),
-                        row![
+                            .on_input(Message::ServerAddrInpuChanged)
+                            ]
+                            .spacing(20),
+                            row![
                             text("Server Password"),
                             TextInput::new("Password", "Some val")
-                                .on_input(Message::ServerAddrInpuChanged)
-                        ]
-                        .spacing(20),
-                        row![
+                            .on_input(Message::ServerAddrInpuChanged)
+                            ]
+                            .spacing(20),
+                            row![
                             text("Server Nickname"),
                             TextInput::new("nickname", "Some val")
-                                .on_input(Message::ServerAddrInpuChanged)
-                        ]
-                        .spacing(20)
-                    ]
-                    .spacing(30),
-                ),
-            )
-            .foot(
-                Row::new()
-                    .spacing(10)
-                    .padding(5)
-                    .width(Length::Fill)
-                    .push(
-                        Button::new(Text::new("Cancel").horizontal_alignment(Horizontal::Center))
-                            .width(Length::Fill)
-                            .on_press(Message::CancelButtonPressed),
-                    )
-                    .push(
-                        Button::new(Text::new("Ok").horizontal_alignment(Horizontal::Center))
-                            .width(Length::Fill)
-                            .on_press(Message::OkButtonPressed),
-                    ),
-            )
-            .max_width(500.0)
-            .on_close(Message::CloseModal);
-            modal_content.into()
-        } else {
-            println!("Open modal closed ");
-            let server_pk_list = {
-                let tags = vec!["Connect".to_string(), "Disconnect".to_string()];
-                let tag_el = pick_list(tags, None::<String>, Message::ServerDropDownAction)
-                    .placeholder("Server");
-                tag_el
-            };
-            let self_pk_list = {
-                let tags = vec!["Mute Self".to_string(), "Defean Self".to_string()];
-                let tag_el = pick_list(tags, None::<String>, Message::SelfDropDownAction)
-                    .placeholder("Self");
-                tag_el
-            };
-
-            let top_bar = row![server_pk_list, self_pk_list].height(50).spacing(10);
-
-            container(top_bar).padding(10).into()
+                            .on_input(Message::ServerAddrInpuChanged)
+                            ]
+                            .spacing(20)
+                            ]
+                            .spacing(30),
+                            ),
+                            )
+                                .foot(
+                                    Row::new()
+                                    .spacing(10)
+                                    .padding(5)
+                                    .width(Length::Fill)
+                                    .push(
+                                        Button::new(
+                                            Text::new("Cancel").horizontal_alignment(Horizontal::Center),
+                                        )
+                                        .width(Length::Fill)
+                                        .on_press(Message::CancelButtonPressed),
+                                    )
+                                    .push(
+                                        Button::new(Text::new("Ok").horizontal_alignment(Horizontal::Center))
+                                        .width(Length::Fill)
+                                        .on_press(Message::OkButtonPressed),
+                                    ),
+                                )
+                                .max_width(400.0)
+                                .max_height(400.0)
+                                .on_close(Message::CloseModal),
+                                )
+                                    .height(Length::Fill)
+                                    .align_x(Horizontal::Center)
+                                    .align_y(Vertical::Center);
+            container(column![top_bar, server_conn_modal]).into()
         }
     }
 }
